67b108a1ed47d633712d0c1d69ae9b79ea875af2
rongqingliu <phpgege@gmail.com>
2023-04-16 10:40:22 +0800

    fix: 删除多余代码

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,5 @@
 #! /usr/bin/env node
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -25,59 +16,12 @@ console.log(figlet_1.default.textSync("Mao Cli Manager"));
 program
     .version(packageData.version)
     .description("Here are some templates for managing projects");
-// .option("-l, --ls [value]", "List directory contens")
-// .option("-m, --mkdir <value>", "Create a directory")
-// .option("-t, --touch <value>", "Create a file");
 program
     .command("create")
     .description("Create Project Template")
     .argument("<app-name>", "project name")
     .action(create_1.create);
 program.parse(process.argv);
-const options = program.opts();
-function listDirContents(filepath) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const files = yield fs_1.default.promises.readdir(filepath);
-            const detailedFilesPromises = files.map((file) => __awaiter(this, void 0, void 0, function* () {
-                const fileDetails = yield fs_1.default.promises.lstat(path_1.default.resolve(filepath, file));
-                const { size, birthtime } = fileDetails;
-                return {
-                    filename: file,
-                    "size(KB)": size,
-                    created_at: birthtime,
-                };
-            }));
-            const detailedFiles = yield Promise.all(detailedFilesPromises);
-            console.table(detailedFiles);
-        }
-        catch (error) {
-            console.error("Error", error);
-        }
-    });
-}
-function createDir(filepath) {
-    if (!fs_1.default.existsSync(filepath)) {
-        fs_1.default.mkdirSync(filepath);
-        console.log(`创建目录 ${filepath} 成功`);
-    }
-}
-function createFile(filepath) {
-    fs_1.default.openSync(filepath, "w");
-    console.log("已创建一个空文件");
-}
-if (options.ls) {
-    const filepath = typeof options.ls === "string" ? options.ls : __dirname;
-    listDirContents(filepath);
-}
-if (options.mkdir) {
-    // createDir(path.resolve(__dirname, options.mkdir));
-    createDir(options.mkdir);
-}
-if (options.touch) {
-    createFile(options.touch);
-    // createFile(path.resolve(__dirname, options.touch));
-}
 // 若没有参数则显示帮助信息
 if (!process.argv.slice(2).length) {
     program.outputHelp();
--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,6 @@ console.log(figlet.textSync("Mao Cli Manager"));
 program
   .version(packageData.version)
   .description("Here are some templates for managing projects");
-// .option("-l, --ls [value]", "List directory contens")
-// .option("-m, --mkdir <value>", "Create a directory")
-// .option("-t, --touch <value>", "Create a file");
 
 program
   .command("create")
@@ -26,54 +23,6 @@ program
   .action(create);
 
 program.parse(process.argv);
-
-const options = program.opts();
-
-async function listDirContents(filepath: string) {
-  try {
-    const files = await fs.promises.readdir(filepath);
-    const detailedFilesPromises = files.map(async (file: string) => {
-      const fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
-      const { size, birthtime } = fileDetails;
-      return {
-        filename: file,
-        "size(KB)": size,
-        created_at: birthtime,
-      };
-    });
-    const detailedFiles = await Promise.all(detailedFilesPromises);
-    console.table(detailedFiles);
-  } catch (error) {
-    console.error("Error", error);
-  }
-}
-
-function createDir(filepath: string) {
-  if (!fs.existsSync(filepath)) {
-    fs.mkdirSync(filepath);
-    console.log(`创建目录 ${filepath} 成功`);
-  }
-}
-
-function createFile(filepath: string) {
-  fs.openSync(filepath, "w");
-  console.log("已创建一个空文件");
-}
-
-if (options.ls) {
-  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
-  listDirContents(filepath);
-}
-
-if (options.mkdir) {
-  // createDir(path.resolve(__dirname, options.mkdir));
-  createDir(options.mkdir);
-}
-
-if (options.touch) {
-  createFile(options.touch);
-  // createFile(path.resolve(__dirname, options.touch));
-}
 
 // 若没有参数则显示帮助信息
 if (!process.argv.slice(2).length) {
